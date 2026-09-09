5e5703f9264e77797791920778edd4527a9be2d7
Nguyen Minh Phu <phu20032k1@gmail.com>
2026-09-09 23:56:38 +0800

    style: load shared report styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ import "./material-workflow.css";
 import "./subcontractor-quantity.css";
 import "./sidebar-overflow.css";
 import "./subcontractor-dashboard.css";
+import "./shared-reports.css";
 
 export const metadata: Metadata = {
   title: "TAILG · Điều hành công trường",
